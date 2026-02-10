d56100e8728e2dc55e02c84f975989e9a2217ead
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-02-10 14:38:46 +0800

    Added list and str division check

--- script.py
+++ script.py
@@ -5,6 +5,10 @@ def sum(a,b):
 def divide(a, b):
     if b == 0:
         raise ValueError("Division by zero!")
+    if isinstance(a, list) or isinstance(b, list):
+        raise ValueError("Could not divide lists")
+    if isinstance(a, list) or isinstance(b, list):
+        raise ValueError("Could not divide lists")
     return a / b
 
 def mul(a, b):
