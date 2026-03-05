8d5d1f44396c4fc1a5668fac462cc4cddb3694ac
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-03-05 13:17:32 +0800

    Show test-clusters-principle

--- testing/principle/tests/test_principle.py
+++ testing/principle/tests/test_principle.py
@@ -27,6 +27,17 @@ def test_addition_overkill():
             assert add(-i, -j) == -i + -j
             assert add(i, -j) == i - j
 
+def test_addition_clusters():
+    assert add(7, 6) == 13
+    assert add(0, 6) == 6
+    assert add(7, 0) == 7
+    assert add(10, -11) == -1
+    assert add(-10, -11) == -21
+    assert add(-5, 0) == -5
+    assert add(0, -2) == -2
+    print("Tests CLUSTERS PASSED")
+
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
