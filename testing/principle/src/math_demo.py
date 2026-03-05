6c5470aa698cbc658852c4d789df4a0af1f68ae4
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-03-05 14:17:21 +0800

    Updated tax_calculator

--- testing/principle/src/math_demo.py
+++ testing/principle/src/math_demo.py
@@ -9,4 +9,6 @@ def tax_calculator_bugged(income):
     return income * 0.15
 
 def tax_calculator(income):
+    if income < 0:
+        raise ValueError("Could not have negative income")
     return round(income * 0.15, 2)
