182f9e94973e287e50bac9332326d9cd17ef80ba
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-03-25 18:39:23 +0800

    Fixed structure

--- testing/tdd/script.sh
+++ testing/tdd/script.sh
@@ -1,4 +1,4 @@
-grep import service.py | awk '{print $2}' > output.txt
+grep import src/service.py | awk '{print $2}' > output.txt
 sort -o output.txt output.txt
 sort -o requirements.txt requirements.txt
 diff -u output.txt requirements.txt | sed '1,2d' | grep "^[+-]" > final.txt
