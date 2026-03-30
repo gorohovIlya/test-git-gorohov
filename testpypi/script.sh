fa4426b2fba21cd43ae16c8f1cc0b3d306fd6f69
Spa12andSp10 <sergey.pleskunov.2006@gmail.com>
2026-03-30 19:59:24 +0800

    Added automatically checking

---
+++ testpypi/script.sh
@@ -0,0 +1,10 @@
+grep import src/taxes.py | awk '{print $2}' > output.txt
+sort -o output.txt output.txt
+sort -o requirements.txt requirements.txt
+diff -u output.txt requirements.txt | sed '1,2d' | grep "^[+-]" > final.txt
+echo "missing libraries:"
+grep "+" final.txt | sed 's/^+//'
+echo "extra libraries:"
+grep "-" final.txt | sed 's/^-//'
+rm final.txt
+rm output.txt
