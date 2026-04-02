492c0faaef6c3e4f8f678925aad73c3282cbae83
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-04-02 15:01:19 +0800

    Added functions from src to documentation

--- docs_domain/docs/source/conf.py
+++ docs_domain/docs/source/conf.py
@@ -14,16 +14,23 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode"
+    ]
 
 source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown"
 }
 
+autosummary_generate = True
+
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
