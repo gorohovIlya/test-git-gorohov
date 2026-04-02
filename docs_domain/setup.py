496f8e16ea899d3eaacdba239fba1518a029c128
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-04-02 13:01:53 +0800

    Created sales project

---
+++ docs_domain/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "sales",
+    version = "0.0.0",
+    long_description = "Calculator of income tax in Russia",
+    long_description_content_type = "text/markdown",
+    author = "Ilya Gorokhov",
+    author_email = "ilya.gorohov.404@gmail.com",
+    license = "MIT",
+    url = "https://github.com/gorohovIlya/test-git-gorohov/tree/feature/testing-tdd#",
+    package_dir = {"": "src"},
+    packages = find_packages(where="src"),
+    python_requires=">=3",
+)
