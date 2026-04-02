1536295c4edde2bcf11b0f574a5647453700c3aa
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-04-02 13:08:56 +0800

    Added processing of float quantity

--- docs_domain/src/sales.py
+++ docs_domain/src/sales.py
@@ -11,6 +11,8 @@ def _parse_sale(line: str):
     try:
         unit_price = float(sale[2])
         quantity = int(sale[3])
+        if int(sale[3]) != float(sale[2]):
+            return None
     except ValueError:
         return None
 
